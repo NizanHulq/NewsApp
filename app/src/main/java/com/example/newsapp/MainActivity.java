b045883634c7f65b9ce2b98ebeaf2a768c86badc
FirmandaY <firmandayahya01@mail.ugm.ac.id>
2022-05-25 10:47:48 +0700

    Progress Backend 1

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.newsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    //NewsAPI = --9ee42810f982424cbc8fb13cd90d85e7--
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
