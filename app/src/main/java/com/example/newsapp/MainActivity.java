0552b7a77573554bcaf71ad146aa7871b71b9377
FirmandaY <firmandayahya01@mail.ugm.ac.id>
2022-06-22 20:29:39 +0700

    Progress Backend News 5

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
 
     private void getCategories(){
         categoryRVModalsArrayList.add(new CategoryRVModal("All","https://images.unsplash.com/photo-1484100356142-db6ab6244067?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"));
-        categoryRVModalsArrayList.add(new CategoryRVModal("Dev.Suggestion","https://images.unsplash.com/photo-1484100356142-db6ab6244067?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHJhbmRvbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"));
+
         categoryRVModalsArrayList.add(new CategoryRVModal("Technology","https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRlY2hub2xvZ2llc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"));
         categoryRVModalsArrayList.add(new CategoryRVModal("Science","https://images.unsplash.com/photo-1628595351029-c2bf17511435?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHNjaWVuY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60"));
         categoryRVModalsArrayList.add(new CategoryRVModal("Sports","https://images.unsplash.com/photo-1574629810360-7efbbe195018?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzV8fHNwb3J0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"));
